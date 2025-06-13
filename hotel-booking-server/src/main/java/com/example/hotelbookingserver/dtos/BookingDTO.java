c1bfe548660e083e4336c466538b96c3f4c16665
Monkeyy <truongcot1907@gmail.com>
2025-06-13 15:33:40 +0700

    fix booking

--- hotel-booking-server/src/main/java/com/example/hotelbookingserver/dtos/BookingDTO.java
+++ hotel-booking-server/src/main/java/com/example/hotelbookingserver/dtos/BookingDTO.java
@@ -1,23 +1,28 @@
 package com.example.hotelbookingserver.dtos;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingDTO {
     private UUID id;
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
     private UUID hotelId;
+    private UUID userId;
     private UUID roomTypeId;
     private String status;
+
+    private HotelDTO hotel;
     private UserDTO user;
     private RoomTypeDTO room;
-
 }
